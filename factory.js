ff40e2853044da0e2e38a77232f2b959a5b22748
tamccullough88 <tamccullough88@gmail.com>
2023-09-12 20:31:37 -0400

    completed tests based off instructions given, diagrams that included error, and the test file. was able to get all tests to pass woth green lights

--- factory.js
+++ factory.js
@@ -1,3 +1,23 @@
 class Factory{
-    
+    constructor(location, owner){
+        this.location = location
+        this.owner = owner
+        this.vehicles = []
+}
+
+createVehicle(vehicle){
+    this.vehicles.push(vehicle)
+
+}
+//paints the matching vehicle the specified color
+
+paintVehicle(vin, paintColor) {
+    let vIndex = this.vehicles.findIndex(v => v.vin === vin)
+    this.vehicles[vIndex].paint(paintColor)
+
+
+}
+listVehicles(){
+    return this.vehicles
+}
 }
